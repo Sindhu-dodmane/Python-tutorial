889eef8614f9e0577f7917ba7ac17b28b53b1c15
sindhu M D <sindhudodmane2003@gmail.com>
2025-08-10 19:44:03 +0530

    add some basics print function in python

--- 1arithmeticopnanddatatypes.py
+++ 1arithmeticopnanddatatypes.py
@@ -1,4 +1,21 @@
+#basic print function in python
 print('sindhu')
+name='sindhudodmane'
+print(name)
+print('python', 'is', 'fun')
+print('python', 'is', 'fun', sep='-')
+
+# by using string farmat
+name = "Sindhu"
+score = 95
+print("{} scored {} marks.".format(name, score))
+print(f"{name} scored {score} marks.")
+
+# escape characters inside print fuction
+print("Hello\nWorld")   # New line
+print("Hello\tWorld")   # Tab space
+print("She said, \"Python is fun!\"")  # Quotes inside string
+
 print(r'this is \n newline') #raw string to ignores newline 
 #a=10
 #b=3
@@ -16,7 +33,7 @@ x=20
 p=None #spl datatype
 print(type(a))
 y=float(a)
-print(type(y))
+print(type(y))  #checking type of variable
 z=str(y)
 print(type(z))
 print(type(p))
