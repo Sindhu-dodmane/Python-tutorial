723cd29ef03442cc8324d479576a537e2eaf8c5f
sindhu M D <sindhudodmane2003@gmail.com>
2025-09-08 23:02:15 +0530

    adding with more examples

--- 18While_loop.py
+++ 18While_loop.py
@@ -2,56 +2,56 @@
 while condition:
       <block of code> '''
 #print the numbers 1 to 10 using while loop
-'''x=1
+x=1
 while x<=10:
     print(x ,end=' ')
     x+=1 
-print("\n") '''
+print("\n") 
     
 #print even numbers between 1 to 50
-'''x=1
+x=1
 while x<50:
       if x%2==0:
             print(x,end=',')
       x = x+1
-print('\n')'''
+print('\n')
 
 #print multiplication table eg,5
-'''x=1
+x=1
 while x<=10:
       print(f'5x{x}= {5*x}')
       x+=1
-print('\n') '''
+print('\n') 
 
 #find factorial of a number
-'''num=int(input('enter a number: '))
+num=int(input('enter a number: '))
 fact=1
 while num>=1:
     fact=fact*num
     num=num-1
-print(fact)'''
+print(fact)
       
       
 #sum of digits
-'''number=int(input('enter a number: '))
+number=int(input('enter a number: '))
 s=0
 while number>0:
       digit=number%10
       s+=digit
       number//=10
-print('sum of digits: ',s)'''
+print('sum of digits: ',s)
 
 #reverse a given number
-'''num=int(input('enter a number: '))
+num=int(input('enter a number: '))
 rev=0
 while num>0:
       digit=num%10
       rev=rev*10+digit
       num//=10
-print('reversed number:',rev)'''
+print('reversed number:',rev)
 
 #same logic for palindrome
-'''num1=int(input('enter a number: '))
+num1=int(input('enter a number: '))
 rev=0
 temp=num1
 while num1>0:
@@ -61,45 +61,71 @@ while num1>0:
 if temp==rev:
       print('palindrome')
 else:
-      print('not a palindrome')'''
+      print('not a palindrome')
 
 #count digit in number
-'''num2=int(input('enter a number: '))
+num2=int(input('enter a number: '))
 count=0
 while num2>0: #123
       num2//=10
       count+=1
-print('number of digits :',count)'''
+print('number of digits :',count)
 
 #find the largest digit in a given number
-'''num3=int(input('enter a number: '))
+num3=int(input('enter a number: '))
 largest=0
 while num3>0:
       digit=num3%10 #985
       if digit>largest:
             largest=digit
       num3//=10 
-print('largest digit is: ',largest) '''
+print('largest digit is: ',largest) 
 
 #print fibonocci series
-'''n=10 # can also take user input
+n=10 # can also take user input
 a,b=0,1
 i=0
 while i<n:
       print(a, end=' ')
       a,b=b,a+b
-      i+=1'''
+      i+=1
 
 #armstrong number
-'''num=574
+num=574
 arm=0
 while num>0:
       digit=num%10
       sqr=digit*digit
       arm+=sqr
       num//=10
-print(arm)'''
+print(arm)
 
+#while loop using break and continue
+x = 1
+while x <= 50:
+    if x % 2 != 0:
+        x = x + 1
+        continue   # skip odd numbers
+    print(x, end=" ")
+    x = x + 1
+ 
+
+#while inside while
+i = 1
+while i < 10:
+    x = 0
+    while x < i:   
+        print(x, end=' ')
+        x = x + 1
+    print()   # newline after each row
+    i = i + 1
+
+       
         
+      
+
+
+            
+                    
 
 
--- 19For_loop.py
+++ 19For_loop.py
@@ -3,76 +3,72 @@ for condition:
     <block of code'''
 
 #print 1 to 10 using for loop
-'''for i in range(1,11): #range(start,end-1,step)
+for i in range(1,11): #range(start,end-1,step)
     print(i,end=' ') 
 print('\n')
 #by using step can print odd numbers
 for i in range(1,50,2):
-    print(i,end=' ')'''
+    print(i,end=' ')
     
 #print square number from 1 to 10
-'''for i in range(1,11):
+for i in range(1,11):
     print(f'square of {i}={i*i}')
 
 string=input('enter a string: ')
 for ch in string:
-    print(ch,end=',')'''
+    print(ch,end=',')
     
 #enumerate function
-'''name='sindhu dodmane'
+name='sindhu dodmane'
 for index,letter in enumerate(name):
-     print(f'the letter "{letter}" is in index {index}\n')'''
+     print(f'the letter "{letter}" is in index {index}\n')
      
 #nested for loop for multiplication table
-'''for i in range(1,11):
+for i in range(1,11):
     for j in range(1,11):
         print(f'{i}x{j}= {i*j}')
         j+=1
-    i+=1 '''
+    i+=1 
     
 #take dictionary as example for enumerate()
-'''my_dict={'jan':1,'feb':2,'march':3}
+my_dict={'jan':1,'feb':2,'march':3}
 for key,value in enumerate(my_dict):
-    print(key,value) '''
+    print(key,value) 
 
 #print even numbers between 1 to 50
-'''for i in range(1,50):
+for i in range(1,50):
     if i%2==0:
-        print(i,end=' ') '''
+        print(i,end=' ') 
         
 #find sum of numbers from 1 to N
-'''n=int(input('enter value of  n:'))
+n=int(input('enter value of  n:'))
 sum=0
 for i in range(1,n+1):
     sum+=i
-print(f'sum of numbers between 1 to {n} is: {sum}')'''
+print(f'sum of numbers between 1 to {n} is: {sum}')
 
 #count the vowels in string
-'''string_1=input('enter a string: ')
+string_1=input('enter a string: ')
 count=0
 for ch in string_1:
     if ch in "aeiou":
         count+=1
-print(f'total number of vowels in given string: {count}')'''
+print(f'total number of vowels in given string: {count}')
 
 #print factorial of given number using for loop
-'''num=int(input('enter a number: '))
+num=int(input('enter a number: '))
 fact=1
 for i in range(1,num):
     fact*=num
     num-=1
-print(f'factorial of given number is : {fact}')'''
+print(f'factorial of given number is : {fact}')
 
 #find largest element in list
-'''my_list=[4,6,8,2,9,3,10]
+my_list=[4,6,8,2,9,3,10]
 largest=0
 for i in my_list:
     if i>largest:
         largest=i
-print(largest) #10'''
+print(largest) #10
 
-#print * pyramid
-rows=5
-col=5
-for i in range(1,rows):
-    print('*'*i,end='\n')
+
